27757aba6641b4bbee423181633c4576b9f3dabf
Damian Midor <dmidor@gmail.com>
2015-09-01 21:52:08 +0200

    klasa glowna. w klasie glownej jest metoda start

---
+++ Service/src/main/java/pl/rodim/service/StartConfiguration.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.rodim.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ *
+ * @author rodim
+ */
+@SpringBootApplication
+
+@Controller
+@RequestMapping("/")
+public class StartConfiguration {
+
+    @RequestMapping(method=RequestMethod.GET)
+    public @ResponseBody String sayHello() {
+        return "Rodim SERVICE";
+    }
+    
+    
+    public static void main(String[] args) {
+        SpringApplication.run(StartConfiguration.class, args);
+    }
+    
+}
