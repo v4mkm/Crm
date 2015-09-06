ba2949065cd2c256de10d25b9fdc3641d2a2b368
Damian Midor <dmidor@gmail.com>
2015-09-06 19:23:27 +0200

    edit

--- Service/src/main/resources/view/app.js
+++ Service/src/main/resources/view/app.js
@@ -2,11 +2,20 @@
 var app = angular.module('myApp', []);
 app.controller('customersController', ['$http', function ($http) {
         var ctrl = this;
+        this.edit = false;
         $http.get("http://localhost:9000/employees")
                 .success(function (data) {
                     ctrl.employees = data;
                 });
+        
         this.editUser = function (employee) {
-            $http.post("http://localhost:9000/employees/add", employee);
+            ctrl.edit = true;  
+//            $http.post("http://localhost:9000/employees/add", employee);
         };
+        
+        this.newUser = function(){
+            console.log("newUser");
+          ctrl.edit = false;  
+        };
+        
     }]);
